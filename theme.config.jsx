f350132ebff7df5c610a245dc7f02dcb003f910b
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-09-12 23:32:58 +0900

    [test]배포시 페이지 안나옴 테스트9

---
+++ theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: "https://github.com/CheerUpTwogether/Piggy",
+  },
+  docsRepositoryBase: 'https://github.com/CheerUpTwogether/cheeruptwogether.github.io',
+  // ... other theme options
+};
